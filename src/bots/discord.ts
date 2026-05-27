515510f75a1b140ccb4913c59f798f19160b18c0
Declan Reid <admin@declan-reid.me>
2026-05-27 10:41:56 +1000

    probably fix pm2 crash error by ensuring discord.ts isn't recognised as an async module

--- src/bots/discord.ts
+++ src/bots/discord.ts
@@ -28,15 +28,17 @@ const commands = [
 
 const rest = new REST({ version: '10' }).setToken(TOKEN);
 
-try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-} catch (error) {
-    console.error(error);
+async function registerCommands() {
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+registerCommands();
 
 
 const client = new Client({
