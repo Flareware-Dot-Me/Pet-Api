915ad1388fe04648741df562762aad2bdf329599
Declan Reid <admin@declan-reid.me>
2026-05-27 10:37:39 +1000

    change bun path

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: "pet.flareware.me",
     script: "src/index.ts",
-    interpreter: "bun",
+    interpreter: "/home/deckyboiii/.bun/bin/bun",
     watch: true,
     ignore_watch: ["node_modules", "bun.lock"]
   }]
